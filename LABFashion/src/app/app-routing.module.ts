c227aa6d27bcae1d6b8b66f3740ca12259aa35be
Murilod <murilo.dircksen@gmail.com>
2023-03-23 09:41:09 -0300

    criando layouts e começando rotas

--- LABFashion/src/app/app-routing.module.ts
+++ LABFashion/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './layouts/content/content.component';
+import { FullComponent } from './layouts/full/full.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentComponent,
+    children:[
+      {path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'
+      },
+    //  {
+     //   path: 'login',
+    //   component:LoginComponent
+
+     // }
+    ]
+  },
+  {
+    path: 'home',
+    component: FullComponent,
+    children: [
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
